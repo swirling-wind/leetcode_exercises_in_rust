7b270a02c8d734724b13efa92b30591cf88115a4
Yao <yaozirui@outlook.com>
2022-10-07 11:46:31 +0800

    Add p0218 & Add p0658

---
+++ src/p0218.rs
@@ -0,0 +1,53 @@
+use std::collections::BinaryHeap;
+use crate::Solution;
+
+impl Solution {
+    pub fn get_skyline(buildings: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut result: Vec<Vec<i32>> = vec![];
+        let mut heap: BinaryHeap<i32> = BinaryHeap::new();
+
+        let mut previous_end: Option<i32> = None;
+
+        for item in buildings {
+            let (start, end, height) = (item[0], item[1], item[2]);
+
+            match previous_end {
+                Some(ending) =>
+                    if ending < start {
+                        let change_height = vec![];
+                        result.push(change_height);
+                    }
+                ,
+                None => {previous_end = Some(end);}
+            };
+
+            match heap.peek() {
+                Some(current) => {
+                    if height > *current {
+                        let change_height = vec![start, height];
+                        result.push(change_height);
+                    }
+                }
+                None => {
+                    let change_height = vec![start, height];
+                    result.push(change_height);
+                }
+            };
+
+            heap.push(height);
+        }
+
+        result
+    }
+}
+
+mod test {
+    #[test]
+    fn p0218() {
+        use crate::Solution;
+        let input: Vec<Vec<i32>> = vec![vec![2, 9, 10], vec![3, 7, 15], vec![5, 12, 12],
+                                        vec![15, 20, 10], vec![19, 24, 8]];
+        let result = Solution::get_skyline(input);
+        dbg!(result);
+    }
+}
--- src/p0658.rs
+++ src/p0658.rs
@@ -1,7 +1,7 @@
 use crate::Solution;
 
 impl Solution {
-    pub fn find_closest_elements(arr: Vec<i32>, mut k: i32, x: i32) -> Vec<i32> {
+    pub fn find_closest_elements(arr: Vec<i32>, k: i32, x: i32) -> Vec<i32> {
         let mut low = 0;
         let mut high = arr.len() as i32 - 1;
         while high - low >= k {
@@ -26,22 +26,6 @@ mod test {
 
         let test_0 = vec![1, 5, 10];
         let output_0 = Solution::find_closest_elements(test_0, 1, 4);
-        dbg!(output_0);
-        //
-        // let test_0 = vec![-1,1,2,4,5,9,12];
-        // let output_0 = Solution::find_closest_elements(test_0, 4, 3);
-        // dbg!(output_0);
-        //
-        // let test_0 = vec![1,3];
-        // let output_0 = Solution::find_closest_elements(test_0, 1,2);
-        // dbg!(output_0);
-        //
-        // let test_1 = vec![1, 2, 2, 3, 4, 5];
-        // let output_1 = Solution::find_closest_elements(test_1, 4, -1);
-        // dbg!(output_1);
-        //
-        // let test_1 = vec![1, 2, 3, 4, 4, 5];
-        // let output_2 = Solution::find_closest_elements(test_1, 4, 9);
-        // dbg!(output_2);
+        assert_eq!(output_0, vec![5]);
     }
 }
